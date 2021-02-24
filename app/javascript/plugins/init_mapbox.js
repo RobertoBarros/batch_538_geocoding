4ca9cf0ca02290f11e5ae3df60188975202c7d39
Roberto Barros <roberto.f.barros@gmail.com>
2021-02-24 11:02:00 -0300

    livecode

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,54 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map')
+
+  if (mapElement) {
+
+    const fitMapToMarkers = (map, markers) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    };
+
+
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+      // style: 'mapbox://styles/robertobarros/cklikxku601i218qskhhhx3qx'
+    });
+
+    map.setLayoutProperty('country-label', 'text-field', ['get','name_pt']);
+
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                        mapboxgl: mapboxgl }));
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+    // Create a HTML element for your custom marker
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '45px';
+    element.style.height = '45px';
+
+    new mapboxgl.Marker(element)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+    });
+
+    fitMapToMarkers(map, markers);
+
+  }
+}
+
+
+export { initMapbox }
